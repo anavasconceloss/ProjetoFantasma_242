d617993546d7eb48139061d0a7285ea1610d73ce
anavasconceloss <ana.vasconcelos@estat.com.br>
2024-09-23 14:10:23 -0300

    Add files via upload

---
+++ Projeto_Fantasma242.R
@@ -0,0 +1,58 @@
+install.packages("tidyverse")
+library(tidyverse)
+
+#Transformação de cada uma das abas da planilha em data frames
+
+df_athena<-as.data.frame(Olimpiadas_2000_2016_Verão_)
+
+df_londres<-as.data.frame(Olimpiadas_2000_2016_Verão_)
+
+df_rio<-as.data.frame(Olimpiadas_2000_2016_Verão_)
+
+df_sydney<-as.data.frame(Olimpiadas_2000_2016_Verão_)
+
+df_beijing<-as.data.frame(Olimpiadas_2000_2016_Verão_)
+
+#Igualando as colunas
+
+colnames(df_athena)[2]<-"Sex"
+colnames(df_athena)[4]<-"Height"
+colnames(df_athena)[5]<-"Weight"
+df_athena$Year<-2004
+
+colnames(df_londres)[1]<-"Names"
+df_londres$Year<-2012
+
+colnames(df_rio)[1]<-"Names"
+colnames(df_rio)[3]<-"Age"
+colnames(df_rio)[4]<-"Height"
+colnames(df_rio)[5]<-"Weight"
+colnames(df_rio)[6]<-"Team"
+df_rio$Year<-2016
+
+
+colnames(df_sydney)[1]<-"Names"
+colnames(df_sydney)[2]<-"Sex"
+colnames(df_sydney)[3]<-"Age"
+colnames(df_sydney)[4]<-"Height"
+colnames(df_sydney)[5]<-"Weight"
+colnames(df_sydney)[6]<-"Team"
+colnames(df_sydney)[7]<-"Sport"
+colnames(df_sydney)[8]<-"Event"
+colnames(df_sydney)[9]<-"Medal"
+df_sydney$Year<-2000
+
+df_beijing$Year<-2008
+df_beijing$Height<-as.numeric(df_beijing$Height)
+
+#Transformando em só um data frame
+
+data<-bind_rows(df_athena, df_beijing, df_londres, df_rio, df_sydney)
+
+#Mudança de lbs p
+
+data$Weight<-data$Weight/2.205
+
+
+
+
